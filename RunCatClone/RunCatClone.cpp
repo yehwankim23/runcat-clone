ea4d691204a19937ada46514881ad3710b7879e2
yehwankim23 <yehwankim23@gmail.com>
2022-01-12 22:51:55 +0900

    Add notification icon

---
+++ RunCatClone/RunCatClone.cpp
@@ -0,0 +1,116 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <Windows.h>
+#include <strsafe.h>
+
+#include "resource.h"
+
+const wchar_t szClass[] = L"RunCatCloneClass";
+const wchar_t szTitle[] = L"RunCat Clone";
+
+HINSTANCE hInst;
+NOTIFYICONDATA nid;
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nShowCmd
+)
+{
+    HANDLE hMutex = CreateMutex(NULL, FALSE, L"RunCatCloneMutex");
+
+    if (!hMutex || GetLastError() == ERROR_ALREADY_EXISTS)
+    {
+        MessageBox(NULL, L"RunCat Clone is already running.", szTitle, MB_ICONWARNING);
+        return 1;
+    }
+
+    hInst = hInstance;
+
+    WNDCLASS wc;
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInst;
+    wc.hIcon = LoadIcon(hInst, MAKEINTRESOURCE(IDI_APP));
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = szClass;
+
+    if (!RegisterClass(&wc))
+    {
+        MessageBox(NULL, L"Call to RegisterClass failed.", szTitle, MB_ICONWARNING);
+        return 1;
+    }
+
+    HWND hwnd = CreateWindow(
+        szClass,
+        szTitle,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInst,
+        NULL
+    );
+
+    if (!hwnd)
+    {
+        MessageBox(NULL, L"Call to CreateWindow failed.", szTitle, MB_ICONWARNING);
+        return 1;
+    }
+
+    ShowWindow(hwnd, SW_HIDE);
+    MSG msg;
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    ReleaseMutex(hMutex);
+    return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:
+    {
+        nid.cbSize = sizeof(NOTIFYICONDATA);
+        nid.hWnd = hwnd;
+        nid.uFlags = NIF_ICON | NIF_TIP | NIF_SHOWTIP;
+        nid.hIcon = LoadIcon(hInst, MAKEINTRESOURCE(IDI_APP));
+        StringCchCopy(nid.szTip, ARRAYSIZE(nid.szTip), szTitle);
+        Shell_NotifyIcon(NIM_ADD, &nid);
+
+        nid.uVersion = NOTIFYICON_VERSION_4;
+        Shell_NotifyIcon(NIM_SETVERSION, &nid);
+        return 0;
+    }
+
+    case WM_DESTROY:
+    {
+        Shell_NotifyIcon(NIM_DELETE, &nid);
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    default:
+    {
+        return DefWindowProc(hwnd, message, wParam, lParam);
+    }
+    }
+}
